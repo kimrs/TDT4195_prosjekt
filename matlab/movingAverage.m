06561cd2f1fa0e002b43b33d42cc01f7f00f5392
Bjørn Bråthen <bjorbrat88@gmail.com>
2013-11-14 13:43:43 +0100

    preprocessing finished. Todo: finish hough circle detection.

--- matlab/movingAverage.m
+++ matlab/movingAverage.m
@@ -1,4 +1,4 @@
 % returns the moving average
 
 function J = movingAverage(I)
-J = conv2(I, ones(3)/9, 'same');
+J = conv2(I, ones(5)/12, 'same');
--- matlab/separateColors.m
+++ matlab/separateColors.m
@@ -7,6 +7,8 @@ redImg      = img(:, :, 1);
 greenImg    = img(:, :, 2);
 blueImg     = img(:, :, 3);
 
+[redImg, greenImg, blueImg] = normalizeColorValues(redImg,greenImg,blueImg);
+
 only_red = redImg - greenImg.*4;
 
 only_green = greenImg;
@@ -25,12 +27,23 @@ only_blue = only_blue - redImg;
 [max_blue, ~] = max(only_blue(:));
 only_blue = only_blue.*(1/max_blue);
 
+only_green = only_green > -0.4;
+only_blue = only_blue > -0.6;
+only_red = only_red > 0.42;
+
+ %only_blue   = medianFilter(only_blue, 4);
+ only_red    = medianFilter(only_red, 4);
+ only_green  = medianFilter(only_green, 4);
+
 only_yellow = redImg - blueImg;
 [max_yellow, ~] = max(only_yellow(:));
 only_yellow = only_yellow.*(1/max_yellow);
 only_yellow = only_yellow + greenImg;
 [max_yellow, ~] = max(only_yellow(:));
 only_yellow = only_yellow.*(1/max_yellow);
+
+only_yellow = only_yellow > 0.9;
+
 
 only_pink = redImg - greenImg;
 [max_pink, ~] = max(only_pink(:));
@@ -39,11 +52,9 @@ only_pink = only_pink + blueImg;
 [max_pink, ~] = max(only_pink(:));
 only_pink = only_pink.*(1/max_pink);
 
-only_green = only_green > 0.1;
-only_blue = only_blue > 0.18;
-only_red = only_red > 0.1;
-only_yellow = only_yellow > 0.8;
+
 only_pink = only_pink > 0.8;
+only_pink   = medianFilter(only_pink, 10);
 
 only_orange = redImg - blueImg;
 only_orange = only_orange - only_green;
@@ -52,13 +63,11 @@ only_orange = only_orange - only_yellow;
 only_orange = only_orange - only_red;
 only_orange = only_orange - only_pink;
 
-only_orange = only_orange > 0.54;
+only_orange = only_orange > 0.45;
 
-only_blue   = medianFilter(only_blue, 8);
-only_red    = medianFilter(only_red, 6);
-only_green  = medianFilter(only_green, 4);
-only_orange = medianFilter(only_orange, 14);
-only_pink   = medianFilter(only_pink, 10);
+
+only_orange = medianFilter(only_orange, 8);
+%only_yellow = medianFilter(only_yellow, 14);
 
 % weird
 
@@ -69,15 +78,17 @@ only_yellow = imfill(only_yellow,'holes');
 only_pink = imfill(only_pink,'holes');
 only_orange = imfill(only_orange,'holes');
 
-only_blue = only_blue & ~only_pink;
-only_pink = only_pink & ~only_red;
-
-only_red = openAndClose(only_red);
-only_green = openAndClose(only_green);
-only_blue = openAndClose(only_blue);
-only_yellow = openAndClose(only_yellow);
-only_pink = openAndClose(only_pink);
-only_orange = openAndClose(only_orange);
+ only_blue = only_blue & ~only_pink;
+ only_pink = only_pink & ~only_red;
+ only_green = only_green & ~only_yellow;
+ 
+ 
+ only_red = openAndClose(only_red);
+ only_green = openAndClose(only_green);
+ only_blue = openAndClose(only_blue);
+ only_yellow = openAndClose(only_yellow);
+ only_pink = openAndClose(only_pink);
+ only_orange = openAndClose(only_orange);
 
 
 
