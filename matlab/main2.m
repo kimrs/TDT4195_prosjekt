0e1757a8d7dff306c2bcb0618e0d6fbed3715803
Bjørn Bråthen <bjorbrat88@gmail.com>
2013-11-14 09:48:49 +0100

    starting to put it together.

--- matlab/main2.m
+++ matlab/main2.m
@@ -1,15 +1,44 @@
 close('all');
-% read in image from file
-originalImg = imread('sweetsA03.png');
 
-% show original
-figure('Name','Original image','NumberTitle','off');imshow(originalImg);
+% the file which we process
+fileToLoad = 'sweetsA03';
+
+% read in image from file
+originalImg = imread([fileToLoad '.png']);
 
 % scale image to 0-1 values
 originalImg = double(originalImg)/255.0;
 
-% do a threshold with moving average filter
-bitmask = optimaThreshold(originalImg);
+%normalizedColors = optimaThreshold(originalImg);
+
+seperateColoredObjects = separateColors(originalImg);
+
+red_sweets = seperateColoredObjects(:,:,1);
+green_sweets = seperateColoredObjects(:,:,2);
+blue_sweets = seperateColoredObjects(:,:,3);
+yellow_sweets = seperateColoredObjects(:,:,4);
+pink_sweets = seperateColoredObjects(:,:,5);
+orange_sweets = seperateColoredObjects(:,:,6);
+
+red_sweets = imfill(red_sweets,'holes');
+green_sweets = imfill(red_sweets,'holes');
+blue_sweets = imfill(blue_sweets,'holes');
+yellow_sweets = imfill(yellow_sweets,'holes');
+pink_sweets = imfill(pink_sweets,'holes');
+orange_sweets = imfill(orange_sweets,'holes');
+
+% merge all maskes to one complete mask
+bitmask = red_sweets | green_sweets | blue_sweets | yellow_sweets | pink_sweets | orange_sweets;
+
+% 
+% % do a threshold with moving average filter
+% bitmask = optimaThreshold(originalImg);
+% 
+% % Closing and opening filters, to remove some 
+% bitmask = imclose(bitmask,strel('disk',1));
+% bitmask = imopen(bitmask,strel('disk',8));
+% bitmask = imopen(bitmask,strel('disk',8));
+% bitmask = imclose(bitmask,strel('disk',1));
 
 % create copy of original
 maskedImage = originalImg;
@@ -19,43 +48,37 @@ for i = 1:1:3
     maskedImage(:,:,i) = double(originalImg(:,:,i).*bitmask);
 end
 
-% Closing and opening filters
-bitmask = imclose(bitmask,strel('disk',1));
-bitmask = imopen(bitmask,strel('disk',8));
-bitmask = imopen(bitmask,strel('disk',8));
-bitmask = imclose(bitmask,strel('disk',1));
-
-figure('Name','Final masked image','NumberTitle','off');imshow(bitmask);
-
-% show seperate RGB layers
-% figure('Name','Channels','NumberTitle','off'); subplot(3,1,1); imshow(originalImg(:,:,1));
-% subplot(3,1,2); imshow(originalImg(:,:,2));
-% subplot(3,1,3); imshow(originalImg(:,:,3));
-
-% convert to gray image values.
-grayImg = rgb2gray(maskedImage);
-
-figure('Name','Gray Image','NumberTitle','off'); imshow(grayImg);
-
 % find sweets
-[c,r] = imfindcircles(bitmask,[30,30]);
+[c,r] = imfindcircles(bitmask,[10,30]);
 
-cc=1:80; 
-rr=cc.';
-
-cx1=40; cy1=40; R1=20;
-
-
-f=@(xx,yy) (xx-cx1).^2+(yy-cy1).^2 <=R1^2; 
- 
-
-circ=bsxfun(f,rr,cc); %Logical map of 2 circles
+% cc=1:80; 
+% rr=cc.';
+% 
+% cx1=40; cy1=40; R1=20;
+% 
+% 
+% f=@(xx,yy) (xx-cx1).^2+(yy-cy1).^2 <=R1^2; 
+%  
+% 
+% circ=bsxfun(f,rr,cc); %Logical map of 2 circles
 
 %C = circlesHough(double(circ),20,21);
-C = circlesHough(double(bitmask),15,25);
+
+C = circlesHough(double(bitmask),10,25);
 %figure(); imshow(l);
 %display detected circles
 figure('Name','Detected sweets','NumberTitle','off'); imshow(originalImg);viscircles(c,r);
+
+% Write images to files.
+imwrite(originalImg,['results/' fileToLoad '_original.png']);
+imwrite(maskedImage,['results/' fileToLoad '_masked.png']);
+imwrite(bitmask,['results/' fileToLoad '_total_mask.png']);
+imwrite(red_sweets,['results/' fileToLoad '_red_sweets.png']);
+imwrite(green_sweets,['results/' fileToLoad '_green_sweets.png']);
+imwrite(blue_sweets,['results/' fileToLoad '_blue_sweets.png']);
+imwrite(yellow_sweets,['results/' fileToLoad '_yellow_sweets.png']);
+imwrite(pink_sweets,['results/' fileToLoad '_pink_sweets.png']);
+imwrite(orange_sweets,['results/' fileToLoad '_orange_sweets.png']);
 
 % write coordinates for the circles to file
 % colorstate, radius, x-pos, y-pos
